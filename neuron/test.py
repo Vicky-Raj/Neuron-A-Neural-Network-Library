72dab59aebdecfa5cc33a1ba15b1746b7643aed0
Vicky-Raj <42846313+Vicky-Raj@users.noreply.github.com>
2018-10-06 22:28:38 +0530

    Update test.py

--- neuron/test.py
+++ neuron/test.py
@@ -1,7 +1,7 @@
 import neuron
 import numpy as np
 
-# And gate
+# And gate NN
 X = np.matrix([[1,1],[1,0],[0,1],[0,0],[1,1],[0,1],[1,1]]) #Train set
 y = np.matrix([1, 0, 0, 0, 1, 0, 1]).T#Train set
 test_x = np.matrix([[0,0],[0,0],[1,1],[0,1]]) #Test set
@@ -13,6 +13,11 @@ model.add(neuron.Layer(5,activation='tanh'))
 model.add(neuron.Layer(2,activation='tanh'))
 model.add(neuron.Layer(1,activation='sigmoid'))
 #Fitting the model
-model.fit(X,y,batch_size=1,print_error=True)
+model.fit(X,y,val_x=X,val_y=y,batch_size=1,print_stats=True)
 #Predicting
-model.predict(test_x)
+print(model.predict(test_x))
+#Saving model
+model.save_parameters('filename')
+#Loading pretrained model and predicting
+model.load_parameters('filename or path+filename')
+print(model.predict(test_x))
